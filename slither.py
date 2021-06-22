ab4bcc9dccca07389dedf2e4cb5eee370d5a4d93
yashsharma2512 <zackgotyorback@gmail.com>
2021-06-22 22:25:58 +0530

    New Python Games

---
+++ slither.py
@@ -0,0 +1,36 @@
+import turtle
+timothy = turtle.Turtle()
+screen = turtle.getscreen()
+screen.screensize(200,200)
+anthony = turtle.Turtle()
+color = input('What color would you like to use for timothy??')
+timothy.color(color)
+color1 =input('What color would you like to use for anthony??')
+anthony.color(color1)
+x = turtle.numinput('NUM','Number')
+y = turtle.numinput('NUM2','Number again')
+anthony.width(x)
+timothy.width(y)
+def up():
+    timothy.forward(100)
+def turn():
+    timothy.right(50)
+turtle.listen()
+turtle.onkey(up,'Up')
+turtle.onkey(turn,'Right')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
